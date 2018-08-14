d6be0d474ae7e55cb7d5b22c0084634ed904b139
angelolvera99 <42361620+angelolvera99@users.noreply.github.com>
2018-08-13 20:36:41 -0500

    Add files via upload

---
+++ Hola mundo Angel Olvera.c
@@ -0,0 +1,24 @@
+/** main.c Este es un arhicov hola mundo
+* Includo solo para evitar
+* la maldicion del programador. En este curso, 
+* usted aprendera a usar correctamente muchos de 
+* los elementos del lenguaje C. Especialemente,usted
+* derbera aprender a usar apuntadores de lo contrario,
+* sufrira lo indecible y un perro salvaje lo perseguira
+* por los pasillos de la UPIITA (si no
+* aprende a usar bien los apuntadores).
+*/
+#include <stdio.h> /* Printf (Char*,...) 
+Este es un ejemplo de comentario de bloque. */
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) //comentario de una sola linea //
+{
+  
+  putchar ('A');
+  putchar ('\n');
+  printf ("HOLA MUNDO C! \n");
+  system ("PAUSE");	
+  return 0;
+ 
+}
